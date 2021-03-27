767baa4e0016a00db6e0975d4150a9d2285af5c1
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-27 14:39:47 -0300

    Added fonts and animations

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -8,13 +8,13 @@ export default class Reviews extends Component {
                 <img src={this.props.data[this.props.current].picture} alt={this.props.data[this.props.current].picture}></img>
                 <div className="review-text">
                     <p className="name">{this.props.data[this.props.current].name}</p>
-                    <p className="role">{this.props.data[this.props.current].role}</p>
+                    <p className="role">{this.props.data[this.props.current].role.toUpperCase()}</p>
                     <p className="desc">{this.props.data[this.props.current].reviewDesc}</p>
                 </div>
                 <div className="functions">
                     <div className="arrows">
-                        <button onClick={this.props.previous}>&#60;</button>
-                        <button onClick={this.props.next}>&#62;</button>
+                        <button id="left" onClick={this.props.previous}>&#60;</button>
+                        <button id="right" onClick={this.props.next}>&#62;</button>
                     </div>
                     <div className="surprise">
                         <button onClick={this.props.random}>Surprise Me</button>
