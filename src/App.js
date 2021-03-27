8e4915409bb1f0422b546bdddacfa375bc484a54
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-27 13:25:41 -0300

    All functions implemented except random review

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import Reviews from './components/Reviews';
-import reviewsData from './reviews.json';
+import data from './reviews.json';
 
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      currentReview: 0
+    }
+
+    this.nextReview = this.nextReview.bind(this);
+    this.previousReview = this.previousReview.bind(this);
+    this.randomReview = this.randomReview.bind(this);
+  }
+
+  nextReview() {
+      if(this.state.currentReview == data.reviews.length -1) {
+        this.setState({
+          currentReview: 0
+        });
+      } else {
+          this.setState({
+            currentReview: ++this.state.currentReview
+          });
+      }
+  }
+
+  previousReview() {
+    if(this.state.currentReview == 0) {
+      this.setState({
+        currentReview: data.reviews.length -1
+      });
+    } else {
+      this.setState({
+        currentReview: --this.state.currentReview
+      });
+    }
+  }
+
+  randomReview() {
+    this.setState({
+      currentReview: 0
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <Reviews data={reviewsData.reviews} />
+        <div className="page-title">
+          <h1>Our Reviews</h1>
+        </div>
+        <Reviews data={data.reviews} current={this.state.currentReview} next={this.nextReview} previous={this.previousReview} random={this.randomReview} />
       </div>
     );
   }
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Reviews extends Component {
+    
     render() {
         return (
-            <div>
-                
+            <div className="review">
+                <img src={this.props.data[this.props.current].picture}></img>
+                <div className="review-text">
+                    <p className="name">{this.props.data[this.props.current].name}</p>
+                    <p className="role">{this.props.data[this.props.current].role}</p>
+                    <p className="desc">{this.props.data[this.props.current].reviewDesc}</p>
+                </div>
+                <div className="functions">
+                    <div className="arrows">
+                        <button onClick={this.props.previous}>&#60;</button>
+                        <button onClick={this.props.next}>&#62;</button>
+                    </div>
+                    <div className="surprise">
+                        <button onClick={this.props.random}>Surprise Me</button>
+                    </div>
+                </div>
             </div>
         )
     }
