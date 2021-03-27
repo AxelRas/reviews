d17c8ebd10c84840daf5d9acf7b9f1c80c48fa14
Axel Rasmussen <axl.rasmussen@gmail.com>
2021-03-27 13:54:32 -0300

    Added surpise me function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import Reviews from './components/Reviews';
 import data from './reviews.json';
 
+function randomReviewPicker(currentReviewInd) {
+  let randomInd = Math.floor(Math.random()*(data.reviews.length));
+  if(randomInd === currentReviewInd) {
+    return randomReviewPicker(currentReviewInd);
+  } else {
+    return randomInd;
+  }
+}
+
 class App extends React.Component {
   constructor() {
     super();
@@ -16,32 +25,32 @@ class App extends React.Component {
   }
 
   nextReview() {
-      if(this.state.currentReview == data.reviews.length -1) {
+      if(this.state.currentReview === data.reviews.length -1) {
         this.setState({
           currentReview: 0
         });
       } else {
           this.setState({
-            currentReview: ++this.state.currentReview
+            currentReview: this.state.currentReview + 1
           });
       }
   }
 
   previousReview() {
-    if(this.state.currentReview == 0) {
+    if(this.state.currentReview === 0) {
       this.setState({
         currentReview: data.reviews.length -1
       });
     } else {
       this.setState({
-        currentReview: --this.state.currentReview
+        currentReview: this.state.currentReview -1
       });
     }
   }
 
   randomReview() {
     this.setState({
-      currentReview: 0
+      currentReview: randomReviewPicker(this.state.currentReview)
     });
   }
 
--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -5,7 +5,7 @@ export default class Reviews extends Component {
     render() {
         return (
             <div className="review">
-                <img src={this.props.data[this.props.current].picture}></img>
+                <img src={this.props.data[this.props.current].picture} alt={this.props.data[this.props.current].picture}></img>
                 <div className="review-text">
                     <p className="name">{this.props.data[this.props.current].name}</p>
                     <p className="role">{this.props.data[this.props.current].role}</p>
